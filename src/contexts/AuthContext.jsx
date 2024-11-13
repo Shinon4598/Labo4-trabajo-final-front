04ca94f04979ead610bb77fd54f45be3d420f1ba
Melina Sanchez <alguien4598@gmail.com>
2024-11-13 12:25:19 -0300

    home

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,3 @@
-// src/contexts/AuthContext.jsx
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -6,6 +5,7 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const login = async (email, password) => {
         try {
@@ -13,11 +13,12 @@ export const AuthProvider = ({ children }) => {
                 email,
                 password,
             });
-    
+
             if (response.data && response.data.user) {
-                const user = response.data.user; // Asegúrate de que esto es un objeto válido
+                const user = response.data.user;
+                user.token = response.data.token;
                 setCurrentUser(user);
-                localStorage.setItem('user', JSON.stringify(user)); // Guarda como JSON
+                localStorage.setItem('user', JSON.stringify(user));
             } else {
                 console.error('Respuesta inesperada:', response.data);
                 throw new Error('Respuesta de inicio de sesión no válida');
@@ -27,7 +28,6 @@ export const AuthProvider = ({ children }) => {
             throw error;
         }
     };
-    
 
     const logout = () => {
         setCurrentUser(null);
@@ -36,7 +36,6 @@ export const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const user = localStorage.getItem('user');
-        console.log('Contenido de localStorage:', user); // Verifica el contenido
 
         if (user) {
             try {
@@ -44,10 +43,13 @@ export const AuthProvider = ({ children }) => {
             } catch (error) {
                 console.error('Error al parsear el usuario:', error);
             }
-        } else {
-            console.log('No hay usuario guardado en localStorage');
         }
+        setLoading(false); // Termina la carga después de procesar el usuario
     }, []);
+
+    if (loading) {
+        return <div>Cargando...</div>; 
+    }
 
     return (
         <AuthContext.Provider value={{ currentUser, login, logout }}>
